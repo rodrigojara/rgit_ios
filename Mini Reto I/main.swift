86b25c17e71fe8902ef3aef4e1aec0e25f4fadb9
rodrigojara <rodrigo.jara.c@icloud.com>
2016-06-10 11:34:54 -0400

    Initial Commit

---
+++ Mini Reto I/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Mini Reto I
+//
+//  Created by Rodrigo Jara on 6/10/16.
+//  Copyright © 2016 Rodrigo Jara. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
