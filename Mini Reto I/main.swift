8c571fba6cded9a66ca6b4d435745717b0d4e040
rodrigojara <rodrigo.jara.c@icloud.com>
2016-06-10 14:24:56 -0400

    Mini Reto I. Uso de ciclo for e If.

--- Mini Reto I/main.swift
+++ Mini Reto I/main.swift
@@ -8,5 +8,26 @@
 
 import Foundation
 
-print("Hello, World!")
-
+// Ciclo for de [0 a 100]
+for i in 0...100{
+    
+    // Si el número es divisible por 5
+    if i%5 == 0{
+        print ("\(i) Bingo!!!")
+    }
+    
+    // Si el número es par
+    if i%2 == 0{
+        print ("\(i) Par")
+    }
+    
+    // Si el número es impar
+    if i%2 != 0{
+        print ("\(i) Impar")
+    }
+    
+    // Si el numero es 30 <= i <= 100
+    if i >= 30 && i <= 40{
+        print ("\(i) Viva Suift!!!")
+    }
+}
